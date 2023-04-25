1602b88a0e1afe9211ee1f12cda402857d58c6fd
infuman69 <saharoy.soham123@gmail.com>
2023-04-26 00:07:19 +0530

    scanner: initialised

--- pages/scanner.tsx
+++ pages/scanner.tsx
@@ -5,6 +5,7 @@ import { User } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { getUserProfile } from "@/utils/UserFunctions";
+import { useZxing } from "react-zxing";
 
 const Scanner = ({
   user,
@@ -14,8 +15,15 @@ const Scanner = ({
   isLoading: boolean;
 }) => {
   const router = useRouter();
+  const [result, setResult] = useState("");
 
   const [loading, setLoading] = useState(true);
+
+  const { ref } = useZxing({
+    onResult(result) {
+      setResult(result.getText());
+    },
+  });
 
   useEffect(() => {
     if (!isLoading) {
@@ -41,7 +49,15 @@ const Scanner = ({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gradient-to-tl from-fuchsia-950 to-black pt-32"></main>
+      <main className="bg-gradient-to-tl from-fuchsia-950 to-black pt-32">
+        <div className="w-100 flex flex-col gap-4 items-center">
+          <video ref={ref} />
+          <p>
+            <span>Last result:</span>
+            <span className="text-white">{result}</span>
+          </p>
+        </div>
+      </main>
     </>
   );
 };
